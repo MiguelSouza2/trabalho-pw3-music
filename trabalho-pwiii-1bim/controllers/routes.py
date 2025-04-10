4433f58989a93cd7c26bee1dd27f55ce92a6bb31
MiguelSouza2 <jubscrebis@gmail.com>
2025-04-10 10:15:09 -0300

    10/04

--- trabalho-pwiii-1bim/controllers/routes.py
+++ trabalho-pwiii-1bim/controllers/routes.py
@@ -2,25 +2,39 @@ from flask import render_template, request, redirect, url_for
 from controllers import musicData
 
 
+# music = musicData.getMusic("era um garoto como eu")
+# musicTitle = music['data'][0]['title']
+# musicLink = music['data'][0]['link']
+# musicArtist = [
+#     music['data'][0]['artist']['name'], music['data'][0]['artist']['picture'] 
+# ]
+# musicPreview = music['data'][0]['preview']
 
-music = musicData.getMusic("era um garoto como eu")
-musicTitle = music['data'][0]['title']
-musicLink = music['data'][0]['link']
-musicArtist = [
-    music['data'][0]['artist']['name'], music['data'][0]['artist']['picture'] 
-]
-musicPreview = music['data'][0]['preview']
+newClassic = musicData.getPopularClassic()
+popularClassicMusics = []
+for i in range(3):
+    popularClassicMusics.append(newClassic['tracks']['data'][i])
+    
+newRock = musicData.getPopularRock()
+popularRockMusics = []
+for i in range(3):
+    popularRockMusics.append(newRock['tracks']['data'][i])
 
+newForro = musicData.getPopularForro()
+popularForroMusics = []
+for i in range(3):
+    popularForroMusics.append(newForro['tracks']['data'][i])
+
+print(popularForroMusics)
 
 def init_app(app):
     @app.route("/")
     def home():
         
         return render_template("index.html",
-                               musicTitle = musicTitle,
-                               musicLink = musicLink, 
-                               musicArtist = musicArtist,
-                               musicPreview = musicPreview
+                               popularClassicMusics=popularClassicMusics,
+                               popularRockMusics=popularRockMusics,
+                               popularForroMusics=popularForroMusics
                                )
     
    
