40e446a3abd669783ab71483c3db514e91fa2475
MiguelSouza2 <jubscrebis@gmail.com>
2025-04-10 10:21:07 -0300

    10/04

--- trabalho-pwiii-1bim/controllers/routes.py
+++ trabalho-pwiii-1bim/controllers/routes.py
@@ -1,14 +1,27 @@
 from flask import render_template, request, redirect, url_for
 from controllers import musicData
 
+# cadmusica
+userlist = [{
+        'Nome' : '',
+        'email' : '',
+        'Senha' :''
+            
+        }]
 
-# music = musicData.getMusic("era um garoto como eu")
-# musicTitle = music['data'][0]['title']
-# musicLink = music['data'][0]['link']
-# musicArtist = [
-#     music['data'][0]['artist']['name'], music['data'][0]['artist']['picture'] 
-# ]
-# musicPreview = music['data'][0]['preview']
+
+
+music = musicData.getMusic("era um garoto como eu")
+musicTitle = music['data'][0]['title']
+musicLink = music['data'][0]['link']
+musicArtist = [
+    music['data'][0]['artist']['name'], music['data'][0]['artist']['picture'] 
+]
+musicPreview = music['data'][0]['preview']
+
+# cadmusica end
+
+# getmusica
 
 newClassic = musicData.getPopularClassic()
 popularClassicMusics = []
@@ -25,7 +38,9 @@ popularForroMusics = []
 for i in range(3):
     popularForroMusics.append(newForro['tracks']['data'][i])
 
-print(popularForroMusics)
+# getmusica end
+
+
 
 def init_app(app):
     @app.route("/")
@@ -37,4 +52,12 @@ def init_app(app):
                                popularForroMusics=popularForroMusics
                                )
     
+    @app.route('/cadmusica', methods=['GET', 'POST'])
+    def cadmusica():
+        if request.method == 'POST':
+            if request.form.get('Nome') and request.form.get('email') and request.form.get('Senha'):
+                userlist.append({'Nome': request.form.get('Nome'), 'email' : request.form.get('email'), 'Senha': request.form.get('Senha')})
+            return redirect(url_for('cadmusica'))
+        return render_template('cadmusica.html',
+                               userlist=userlist)
    
